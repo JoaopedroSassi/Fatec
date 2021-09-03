fff1306edc9e204716500f984012f8446f8d8283
João Pedro Sassi Granado <joao.granado@fatec.sp.gov.br>
2021-09-03 07:20:10 -0300

    Mod EX03 + Ex04 - Aula 5

--- 2_Semestre/linguagem de programacao/Aula 05/ex03 - tam.c
+++ 2_Semestre/linguagem de programacao/Aula 05/ex03 - tam.c
@@ -6,6 +6,7 @@
 int main(){
    setlocale(LC_ALL,"Portuguese");
 
-   int n = 10;
-   printf("%ld", sizeof(int));
+   int vet[] = {1, 6, 7, 9, 1};
+   int tam_vet = (sizeof(vet)/sizeof(int));
+   printf("O tamanho do vetor vet é: %d", tam_vet);
 }
