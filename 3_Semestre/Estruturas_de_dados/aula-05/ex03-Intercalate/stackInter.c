12367a093ca37ba0cd46e79bc890443609e1c6a0
João Pedro Sassi Granado <joaopedro10813@gmail.com>
2022-03-18 19:18:02 -0300

    Update on Intercalate - Solving the logic problem

--- 3_Semestre/Estruturas_de_dados/aula-05/ex03-Intercalate/stackInter.c
+++ 3_Semestre/Estruturas_de_dados/aula-05/ex03-Intercalate/stackInter.c
@@ -5,34 +5,40 @@
 Stack *stackIntercalate(Stack *st1, Stack *st2)
 {
    float value = 0;
-   int sizeStacks = 0;
+   int sizeStack1 = 0, sizeStack2 = 0;
 
    Stack *aux1 = createStack();
    while(!emptyStack(st1))
    {
       push(aux1, pop(st1));
-      sizeStacks++;
+      sizeStack1++;
    }
 
    Stack *aux2 = createStack();
    while(!emptyStack(st2))
    {
       push(aux2, pop(st2));
-      sizeStacks++;
+      sizeStack2++;
    }
 
    Stack *final = createStack();
-   while(sizeStacks > 0)
+   while((sizeStack1 + sizeStack2) > 0)
    {
-      value = pop(aux1);
-      push(final, value);
-      push(st1, value);
-
-      value = pop(aux2);
-      push(final, value);
-      push(st2, value);
-
-      sizeStacks -= 2;
+      if (sizeStack1 != 0)
+      {
+         value = pop(aux1);
+         push(final, value);
+         push(st1, value);
+         sizeStack1--;
+      }
+      
+      if (sizeStack2 != 0)
+      {
+         value = pop(aux2);
+         push(final, value);
+         push(st2, value);
+         sizeStack2--;
+      }
    }
    return final;
 }
@@ -41,12 +47,12 @@ void main(){
    Stack *stack = createStack();
    Stack *stack2 = createStack();
 
-   for (int i = 1; i <= 5; i++)
+   for (int i = 1; i <= 10; i++)
    {
       push(stack, i);
    }
 
-   for (int i = 1; i <= 5; i++)
+   for (int i = 1; i <= 2; i++)
    {
       push(stack2, i+10);
    }
