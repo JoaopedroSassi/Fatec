8b37712ad59565abffe1866af3c90e8e5a311e74
João Pedro Sassi Granado <joaopedro10813@gmail.com>
2022-03-23 20:31:35 -0300

    FINALLY exercise 01 working

--- 3_Semestre/Estruturas_de_dados/listas/1/Ex01/ex01-Occurrences.c
+++ 3_Semestre/Estruturas_de_dados/listas/1/Ex01/ex01-Occurrences.c
@@ -3,59 +3,32 @@
 
 void ocurrences(char *p)
 {
-   char *aux = p;
-
    int sizePhrase = 0;
-   for (int i = 0; aux[i] != '\0'; i++)
+   for (int i = 0; p[i] != '\0'; i++)
       sizePhrase++;
 
-   char letters[sizePhrase];
-   for(int i = 0; aux[i] != '\0'; aux++)
+   char aux[sizePhrase];
+   for (int i = 0; i < sizePhrase; i++)
+      aux[i] = p[i];
+
+   for(int i = 0; i < sizePhrase; i++)
    {
-      char letter = aux[i];
-      int isInLetters = 0;
-      
-      for (int j = 0; j < sizePhrase; j++)
+      char letter = p[i];
+      int amount = 1;
+
+      for (int j = i+1; j < sizePhrase; j++)
       {
-         if (letter == letters[j])
+         if(aux[i] != '-')
          {
-            isInLetters = 1;
-            break;
-         }  
-
-
-
-      }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      
-
-      int amount = 0;
-      for (int j = 0; j < p[i] != '\0'; j++)
-      {
-         if (letter == p[i])
-            amount++;
+            if (letter == p[j])
+            {
+               amount++;
+               aux[j] = '-';
+            }
+         }
       }
       
-      if(isInLetters != 1)
+      if(aux[i] != '-')
          printf("The letter %c appeared %d times\n", letter, amount);
    }
 }
