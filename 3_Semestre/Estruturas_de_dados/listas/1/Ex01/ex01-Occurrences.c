5e811622c5db37f89054ff55a134aa7a716e79d3
João Pedro Sassi Granado <joaopedro10813@gmail.com>
2022-03-23 07:24:41 -0300

    Exercisse 01 still not working

--- 3_Semestre/Estruturas_de_dados/listas/1/Ex01/ex01-Occurrences.c
+++ 3_Semestre/Estruturas_de_dados/listas/1/Ex01/ex01-Occurrences.c
@@ -3,38 +3,68 @@
 
 void ocurrences(char *p)
 {
+   char *aux = p;
+
    int sizePhrase = 0;
-   for(int i = 0; p[i] != '\0'; p++)
+   for (int i = 0; aux[i] != '\0'; i++)
       sizePhrase++;
 
    char letters[sizePhrase];
-   for(int i = 0; p[i] != '\0'; p++)
+   for(int i = 0; aux[i] != '\0'; aux++)
    {
-      char letter = p[i];
+      char letter = aux[i];
+      int isInLetters = 0;
+      
       for (int j = 0; j < sizePhrase; j++)
       {
-         if(letters[j] == letter)
+         if (letter == letters[j])
          {
-
+            isInLetters = 1;
+            break;
          }  
-      }
-      int amount = 1;
-      for (int j = 0; p[j] != '\0'; j++)
-      {
-         if(p[j+1] == letter)
-            amount++;
-         
+
+
+
       }
 
-      printf("The letter %c appeared %d times\n", letter, amount);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+
+      int amount = 0;
+      for (int j = 0; j < p[i] != '\0'; j++)
+      {
+         if (letter == p[i])
+            amount++;
+      }
+      
+      if(isInLetters != 1)
+         printf("The letter %c appeared %d times\n", letter, amount);
    }
 }
 
 void main()
 {
-   char furniture[] = "arara";
+   char animal[] = "arara";
 
-   ocurrences(furniture);
+   ocurrences(animal);
 
    printf("\n");
    system("pause");
