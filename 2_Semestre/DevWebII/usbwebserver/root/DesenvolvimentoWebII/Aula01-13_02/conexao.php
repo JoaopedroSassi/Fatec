6e90a12df6f9bdc106e146573148b7104d28d3d7
João Pedro Sassi Granado <54276982+JoaopedroSassi@users.noreply.github.com>
2023-02-13 21:21:01 -0300

    Create conexao.php

---
+++ 2_Semestre/DevWebII/usbwebserver/root/DesenvolvimentoWebII/Aula01-13_02/conexao.php
@@ -0,0 +1,11 @@
+<?php
+
+	/*$user = "root";
+	$pass = "usbw";
+	$db = "crudphp";
+	$host = "localhost";
+	$conectDB = mysqli_connect($host, $user, $pass) or die(mysqli_error());
+
+	mysql_select_db($db) or die("Erro ao conectar!");*/
+
+?>
