99ff6e080e821a5e1b6df8b1e7e1f223deb1487b
João Pedro Sassi Granado <54276982+JoaopedroSassi@users.noreply.github.com>
2023-02-13 21:35:10 -0300

    Descomentando conexao.php

--- 2_Semestre/DevWebII/usbwebserver/root/DesenvolvimentoWebII/Aula01-13_02/conexao.php
+++ 2_Semestre/DevWebII/usbwebserver/root/DesenvolvimentoWebII/Aula01-13_02/conexao.php
@@ -1,11 +1,10 @@
 <?php
 
-	/*$user = "root";
+	$user = "root";
 	$pass = "usbw";
 	$db = "crudphp";
 	$host = "localhost";
 	$conectDB = mysqli_connect($host, $user, $pass) or die(mysqli_error());
 
-	mysql_select_db($db) or die("Erro ao conectar!");*/
-
+	mysql_select_db($db) or die("Erro ao conectar!");
 ?>
