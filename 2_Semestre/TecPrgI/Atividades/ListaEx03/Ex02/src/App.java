fd97cbabc3d5905692f190c14bcc7f172a58f216
João Pedro Sassi Granado <54276982+JoaopedroSassi@users.noreply.github.com>
2023-02-24 20:12:27 -0300

    Update ex02 - lista 03

--- 2_Semestre/TecPrgI/Atividades/ListaEx03/Ex02/src/App.java
+++ 2_Semestre/TecPrgI/Atividades/ListaEx03/Ex02/src/App.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception {	
+		List<Integer> list = new ArrayList<>();	
+		try {
+			int fim = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite o tamanho da sequência: ", "Fibonacci", JOptionPane.INFORMATION_MESSAGE));
+
+			int n1 = 1;
+			int n2 = 0;
+			for (int i = 0; i < fim; i++) {
+				int n3 = n1 + n2;
+				list.add(n3);
+				n1 = n2;
+				n2 = n3;
+			}
+
+			JOptionPane.showMessageDialog(null, list);
+		} catch (NumberFormatException e) {
+			JOptionPane.showMessageDialog(null, "Exceção: " + e);
+		}
     }
 }
